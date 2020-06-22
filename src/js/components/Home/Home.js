60fe858f3c9c18591de313686c424ba4ad456e6d
Jackson Darlow <jackson.e.darlow@gmail.com>
2020-06-22 16:09:41 +1200

    CI
    Added CircleCI config
    Resolved linting errors

--- src/js/components/Home/Home.js
+++ src/js/components/Home/Home.js
@@ -1,26 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
-class Home extends Component
-{
-  render() {
-    return (
-      <>
-        <h1>
-          <FormattedMessage
-            id="shredindex.app.TITLE"
-            defaultMessage="Shred Index"
-          />
-        </h1>
-        <p>
-          <FormattedMessage
-            id="shredindex.app.SUBTITLE"
-            defaultMessage="Live your best adventure lifestyle"
-          />
-        </p>
-      </>
-    );
-  }
-}
+const Home = () => (
+  <>
+    <h1>
+      <FormattedMessage
+        id="shredindex.app.TITLE"
+        defaultMessage="Shred Index"
+      />
+    </h1>
+    <p>
+      <FormattedMessage
+        id="shredindex.app.SUBTITLE"
+        defaultMessage="Live your best adventure lifestyle"
+      />
+    </p>
+  </>
+);
 
 export default Home;
