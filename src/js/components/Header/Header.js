ef47af4361c2efeed2310eba402ff0502db80054
Thomas Hansen <thomas.andrew.hansen@gmail.com>
2020-09-21 19:54:33 +1000

    Adder Header with Dropdown options

---
+++ src/js/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CNavbar from '@coreui/react/lib/navbar/CNavbar';
+
+const Header = () => (
+  <>
+    <CNavbar></CNavbar>
+  </>
+);
+
+export default Header;
--- src/js/components/Home/Home.js
+++ src/js/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import Header from '../Header/Header';
 
 const Home = () => (
   <>
