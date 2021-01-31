e256571e6535c363255845664599b0ddf74005e1
Thomas Hansen <thomas.andrew.hansen@gmail.com>
2021-01-31 20:25:51 +1000

    Added more tests - needs to be tested

--- cypress/integration/Home/Header.spec.js
+++ cypress/integration/Home/Header.spec.js
@@ -8,7 +8,34 @@ describe('Header', () => {
   it('Has a toggler button', () => {
     cy.get('.c-header-toggler').get('.c-header-toggler-icon');
   });
-  it('Has a settings button', () => {
-    cy.get('.c-subheader-nav-link').contains('Settings');
+
+
+  context('mobile resolution', () => {
+    beforeEach(() => {
+      // run these tests as if in a desktop
+      // browser with a 720p monitor
+      cy.viewport(990, 720);
+    });
+    it('Mobile Hidden', () => {
+      cy.get('.d-md-down-none').should('not.be.visible');
+    });
+    it('Full resolution shown', () => {
+      cy.get('.d-lg-none').should('be.visible');
+    });
+  });
+
+  context('full resolution', () => {
+    beforeEach(() => {
+      // run these tests as if in a mobile browser
+      // and ensure our responsive UI is correct
+      cy.viewport(1280, 720);
+    });
+
+    it('Mobile Hidden', () => {
+      cy.get('.d-md-down-none').should('be.visible');
+    });
+    it('Full resolution shown', () => {
+      cy.get('.d-lg-none').should('not.be.visible');
+    });
   });
 });
