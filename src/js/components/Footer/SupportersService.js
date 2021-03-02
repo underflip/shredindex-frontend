e26f13949d5a2e446915579d2e50df4615b20772
Thomas Hansen <thomas.andrew.hansen@gmail.com>
2021-03-02 18:28:44 +1000

    Added support service

---
+++ src/js/components/Footer/SupportersService.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const SupportService = [
+  {
+    _tag: 'FooterSupportItem',
+    name: 'Reddit',
+    path: 'https://www.reddit.com/r/skiing/comments/dbafm7/so_i_have_been_building_this_what_do_you_guys',
+    imgSrc: 'https://shredindex.com/storage/app/media/shred-index-reddit_logo_small_1.png',
+    exact: true,
+  },
+  {
+    _tag: 'FooterSupportItem',
+    name: 'ProductHunt',
+    path: 'https://www.producthunt.com/posts/shred-index',
+    imgSrc: 'https://shredindex.com/storage/app/media/shred-index-ph-logo-1.png',
+    exact: true,
+  },
+  {
+    _tag: 'FooterSupportItem',
+    name: 'TetonGravityResearch',
+    path: 'https://www.tetongravity.com/story/news/ski-bum-life-affordability-and-more-an-index',
+    imgSrc: 'https://shredindex.com/storage/app/media/Tetongravitylogo-small.png',
+    exact: true,
+  },
+  {
+    _tag: 'FooterSupportItem',
+    name: 'WorldNomads',
+    path: 'https://www.worldnomads.com/Turnstile/AffiliateLink?partnerCode=thomash&source=link&utm_source=thomash&utm_content=weblink&path=//www.worldnomads.com/ski-snowboard-travel-insurance',
+    imgSrc: 'https://shredindex.com/storage/app/media/shred-index_world_nomads_logo_long_grey_RGB_small.png',
+    exact: true,
+  },
+  {
+    _tag: 'FooterSupportItem',
+    name: 'SafteyWing',
+    path: 'https://www.safetywing.com/a/shredindex-insurance',
+    imgSrc: 'https://shredindex.com/storage/app/media/Shred-Index-SafetyWing-Logo-SW-Black.png',
+    exact: true,
+  },
+];
+
+export default SupportService;
