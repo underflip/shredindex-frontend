4ce4e831c1c5e9b8ce06c0e4cdd6c99a74780dba
Thomas Hansen <thomas.andrew.hansen@gmail.com>
2021-03-13 19:06:48 +1000

    Fixed Sidebar Route Links

--- src/js/components/Sidebar/SidebarItems.js
+++ src/js/components/Sidebar/SidebarItems.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CIcon from '@coreui/icons-react';
+import { CNavLink } from '@coreui/react';
 import NavConfig from '../config/nav-config';
 
 const SidebarItems = () => {
@@ -7,10 +8,10 @@ const SidebarItems = () => {
 
   return filtered.map((item) => (
     <li key={item.path} className="c-sidebar-nav-item">
-      <a href={item.path} className="c-sidebar-nav-link">
+      <CNavLink to={item.path} className="c-sidebar-nav-link">
         <CIcon content={item.icon} className="mr-2" />
         {item.name}
-      </a>
+      </CNavLink>
     </li>
   ));
 };
