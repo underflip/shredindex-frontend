fd83c10b64017b47b0f361d831182cc9a59e68c1
Thomas Hansen <thomas.andrew.hansen@gmail.com>
2021-03-24 18:25:47 +1000

    Removed Weather Carousel

--- src/js/components/ResortSingle/ResortSingle.js
+++ src/js/components/ResortSingle/ResortSingle.js
@@ -3,13 +3,11 @@ import ResortSingleNav from '../ResortSingleNav/ResortSingleNav';
 import Jumbotron from '../Jumbotron/Jumbotron';
 import HeroImageCarousel from '../HeroImageCarousel/HeroImageCarousel';
 import HeroStatCarousel from '../HeroStats/HeroStatCarousel';
-import WeatherCarousel from '../WeatherCarousel/WeatherCarousel';
 
 const ResortSingle = () => (
   <>
     <Jumbotron />
     <HeroImageCarousel />
-    <WeatherCarousel />
     <HeroStatCarousel />
     <ResortSingleNav />
   </>
