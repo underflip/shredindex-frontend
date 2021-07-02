441f51842ed3e18444a9d9a7e452043e3d855446
Jackson Darlow <jackson.e.darlow@gmail.com>
2021-07-02 14:46:46 +1200

    Only run coverage tooling in CircleCI https://trello.com/c/UUdr5lO5

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -15,20 +15,24 @@
 const browserify = require('@cypress/browserify-preprocessor');
 
 module.exports = (on, config) => {
-  // Code coverage plugin sends collected results
-  // using its own cy.tasks calls
-  // eslint-disable-next-line global-require
-  require('@cypress/code-coverage/task')(on, config);
+  if (process.env.NODE_ENV === 'e2e') {
+    // Code coverage plugin sends collected results
+    // using its own cy.tasks calls
+    // eslint-disable-next-line global-require
+    require('@cypress/code-coverage/task')(on, config);
 
-  // We tried using the cypress coverage Instrumentation approach
-  // with @cypress/code-coverage/use-browserify-istanbul to avoid
-  // conflicting with Jest (see https://github.com/cypress-io/code-coverage#alternative-for-unit-tests)
-  // but had no luck, so let's use Browserify directly:
+    // We tried using the cypress coverage Instrumentation approach
+    // with @cypress/code-coverage/use-browserify-istanbul to avoid
+    // conflicting with Jest (see https://github.com/cypress-io/code-coverage#alternative-for-unit-tests)
+    // but had no luck, so let's use Browserify directly:
 
-  // Tell cypress to use .bablerc when bundling spec code
-  const options = browserify.defaultOptions;
-  options.browserifyOptions.transform[1][1].babelrc = true; // @todo Find a nicer way to do this
-  on('file:preprocessor', browserify(options));
+    // Tell cypress to use .bablerc when bundling spec code
+    // https://github.com/cypress-io/cypress-browserify-preprocessor#modifying-default-options
+    const options = browserify.defaultOptions;
+    options.browserifyOptions.transform[1][1].babelrc = true;
+
+    on('file:preprocessor', browserify(options));
+  }
 
   if (config.env.baseUrl !== undefined) {
     // eslint-disable-next-line no-param-reassign
