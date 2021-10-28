b102ca10b3714b8ea19bdfb290ca1767c04ce808
Tom <thomas.andrew.hansen@gmail.com>
2021-10-28 17:40:28 +1000

    Button Stories Cleanup

--- src/stories/coreui/Colors.stories.js
+++ src/stories/coreui/Colors.stories.js
@@ -80,42 +80,6 @@ export const Colors = () => (
           </CRow>
         </div>
       </div>
-      <div className="card">
-        <div className="card-header">
-          Grays
-        </div>
-        <div className="card-body">
-          <CRow className="mb-3">
-            <ThemeColor className="bg-gray-100">
-              <h6>Gray 100 Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-gray-200">
-              <h6>Gray 200 Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-gray-300">
-              <h6>Gray 300 Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-gray-400">
-              <h6>Gray 400 Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-gray-500">
-              <h6>Gray 500 Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-gray-600">
-              <h6>Gray 600 Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-gray-700">
-              <h6>Gray 700 Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-gray-800">
-              <h6>Gray 800 Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-gray-900">
-              <h6>Gray 900 Color</h6>
-            </ThemeColor>
-          </CRow>
-        </div>
-      </div>
     </div>
   </>
 );
