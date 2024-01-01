3c865146aa00aa5bfc310fb2a177249bf3d6aa74
Thomas A Hansen <45204791+Krank3n@users.noreply.github.com>
2024-01-01 18:14:46 +1300

    changed index to be browsify

--- src/js/index.js
+++ src/js/index.js
@@ -17,7 +17,7 @@ import '@fontsource/plus-jakarta-sans';
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
-    uri: process.env.GRAPHQL_URI,
+    uri: 'http://localhost:8080/graphql',
   }),
 });
 
