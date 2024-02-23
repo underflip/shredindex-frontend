729d933295e7e4bd2ced2e658d27c81e176dd5c4
Thomas A Hansen <45204791+Krank3n@users.noreply.github.com>
2024-02-23 15:09:38 +1000

    Added more tests

--- cypress/e2e/RankedResortFilter/RankedResortFilters.cy.js
+++ cypress/e2e/RankedResortFilter/RankedResortFilters.cy.js
@@ -15,5 +15,43 @@ describe('Filters', () => {
     it('Shows a title Features', () => {
       cy.get('.filters__features').should('contain.text', 'Must have features');
     });
+
+    it('Toggles show more/less text on button click', () => {
+      let button = cy.get('#showMoreRatings');
+      button.should('contain.text', 'Show more ratings +');
+      button.click();
+      button.should('contain.text', 'Show less ratings -');
+      button.click();
+      button.should('contain.text', 'Show more ratings +');
+    });
+  });
+
+  context('All devices', () => {
+    it('Verifies if Score Filter Toggles are present', () => {
+      cy.get('.filters__scores').should('have.length.gt', 0);
+    });
+
+    it('Verifies if Stats Filter Toggles are present', () => {
+      cy.get('.filters__stats').should('have.length.gt', 0);
+    });
+
+    it('Verifies if Features Filter Toggles are present', () => {
+      cy.get('.filters__features').should('have.length.gt', 0);
+    });
+
+    it('Verifies if Show More Ratings Button toggles more content', () => {
+      cy.get('#showMoreRatings').click();
+      cy.get('#showMoreRatings').should('contain.text','Show less ratings -');
+    });
+
+    it('Verifies if Show More Stats Button toggles more content', () => {
+      cy.get('#showMoreNumeric').click();
+      cy.get('#showMoreNumeric').should('contain.text','Show less stats -');
+    });
+
+    it('Verifies if Show More Features Button toggles more content', () => {
+      cy.get('#showMoreGenerics').click();
+      cy.get('#showMoreGenerics').should('contain.text','Show less features -');
+    });
   });
 });
--- src/js/hooks/tests/useLocalStorageDrivenBooleanState.test.js
+++ src/js/hooks/tests/useLocalStorageDrivenBooleanState.test.js
@@ -53,4 +53,78 @@ describe('useLocalStorageDrivenBooleanState hook', () => {
     await act(wait);
     expect(localStorage.getItem(localStorageKey)).toBe('');
   });
+
+  it('Generates the new state correctly, omitting the current id if the boolean flag is false', async () => {
+    let store;
+    const testId = '1';
+    const otherIds = '2_3_4';
+    localStorage.setItem(localStorageKey, `${testId}_${otherIds}`);
+
+    const TestHook = () => {
+      const [stored, setStored] = useLocalStorageDrivenBooleanState(localStorageKey, testId);
+      store = setStored;
+      return stored;
+    };
+
+    mount(<TestHook />);
+
+    await act(async () => {
+      store(false);
+    });
+
+    expect(localStorage.getItem(localStorageKey)).toBe(otherIds);
+  });
+
+  it('Generates the new state correctly, including the current id if the boolean flag is true', async () => {
+    let store;
+    const testId = '1';
+    const otherIds = '2_3_4';
+    localStorage.setItem(localStorageKey, otherIds);
+
+    const TestHook = () => {
+      const [stored, setStored] = useLocalStorageDrivenBooleanState(localStorageKey, testId);
+      store = setStored;
+      return stored;
+    };
+
+    mount(<TestHook />);
+
+    await act(async () => {
+      store(true);
+    });
+
+    expect(localStorage.getItem(localStorageKey)).toBe(`${otherIds}_${testId}`);
+  });
+
+  it('Retrieves from the stored state correctly', () => {
+    let actual;
+    const testId = '1';
+    const otherIds = '2_3_4';
+    localStorage.setItem(localStorageKey, `${testId}_${otherIds}`);
+
+    const TestHook = () => {
+      actual = useLocalStorageDrivenBooleanState(localStorageKey, testId);
+      return null;
+    };
+
+    mount(<TestHook />);
+
+    expect(actual[0]).toBe(true);
+  });
+
+  it('Retrieves correctly and indicates that the id is not in the stored state', () => {
+    let actual;
+    const testId = '1';
+    const otherIds = '2_3_4';
+    localStorage.setItem(localStorageKey, otherIds);
+
+    const TestHook = () => {
+      actual = useLocalStorageDrivenBooleanState(localStorageKey, testId);
+      return null;
+    };
+
+    mount(<TestHook />);
+
+    expect(actual[0]).toBe(false);
+  });
 });
