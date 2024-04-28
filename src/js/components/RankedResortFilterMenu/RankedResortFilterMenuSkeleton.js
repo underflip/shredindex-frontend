cbffee88828a827232d963720187030a2d0c7c12
Thomas A Hansen <45204791+Krank3n@users.noreply.github.com>
2024-04-28 16:08:35 +1000

    Added in continent

--- src/js/components/RankedResortFilterMenu/RankedResortFilterMenuSkeleton.js
+++ src/js/components/RankedResortFilterMenu/RankedResortFilterMenuSkeleton.js
@@ -4,6 +4,7 @@ import ButtonSkeleton from '../SkeletonState/ButtonSkeleton';
 const RankedResortFilterMenuSkeleton = () => (
   <div className="filter-menu mb-4 d-flex flex-row gap-4">
     <ButtonSkeleton className="me-2" />
+    <ButtonSkeleton className="me-2" />
     <ButtonSkeleton className="ms-2" />
   </div>
 );
