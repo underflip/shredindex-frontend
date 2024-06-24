b329a52c78af22701c2ff832802b83810716d895
Thomas A Hansen <45204791+Krank3n@users.noreply.github.com>
2024-06-24 17:07:07 +1000

    Fixed hero and heading positioning

--- src/js/components/Hero/Hero.js
+++ src/js/components/Hero/Hero.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import { CButton } from '@coreui/react';
 import { ReactComponent as HeroDesktop } from '../../../images/Hero-Desktop.svg';
+import { ReactComponent as HeroTablet } from '../../../images/Hero-Tablet.svg';
 import { ReactComponent as HeroMobile } from '../../../images/Hero-Mobile.svg';
-import {
-  CButton,
-  CDropdown,
-  CDropdownDivider,
-  CDropdownItem,
-  CDropdownMenu,
-  CDropdownToggle
-} from '@coreui/react';
 
 const Hero = () => (
   <div className="hero">
     <div className="hero__image">
-      <HeroDesktop />
-      {/* <HeroDesktop /> */}
+      <div className="hero__svg hero__svg--desktop">
+        <HeroDesktop />
+      </div>
+      <div className="hero__svg hero__svg--tablet">
+        <HeroTablet />
+      </div>
+      <div className="hero__svg hero__svg--mobile">
+        <HeroMobile />
+      </div>
     </div>
     <div className="hero__heading">
       <h1>
@@ -24,10 +25,10 @@ const Hero = () => (
           defaultMessage="Live your best adventure lifestyle."
         />
       </h1>
-    </div>
-    <div className="d-flex align-items-start">
-      <CButton label={'Explore resorts'} className="me-2" variant="">Explore resorts</CButton>
-      <CButton label="Become a member" className="me-2" variant="">Become a member</CButton>
+      <div className="button-group d-flex align-items-start">
+        <CButton className="me-2" color="secondary" variant="">Explore resorts</CButton>
+        {/* <CButton className="me-2" color="warning" variant="">Become a member</CButton> */}
+      </div>
     </div>
   </div>
 );
