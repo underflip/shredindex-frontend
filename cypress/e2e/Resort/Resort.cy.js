1be913c30f11afb1955554ed233903f58fb402a8
Thomas A Hansen <45204791+Krank3n@users.noreply.github.com>
2024-07-05 22:35:28 +1000

    Organising the resort page Added in 3D map Keep position of filter model Updated structure for resortAverage ratings/ rating scores

--- cypress/e2e/Resort/Resort.cy.js
+++ cypress/e2e/Resort/Resort.cy.js
@@ -11,7 +11,7 @@ const dataProvider = {
       name: 'Dont have a State ey bro',
     },
   },
-  ratings: [
+  ratingScores: [
     {
       title: 'Digital Nomad Score',
       value: 76,
@@ -29,6 +29,24 @@ const dataProvider = {
       value: 'true',
     },
   ],
+  comments: [
+    {
+      author: 'John Doe',
+      comment: 'Amazing place!',
+    },
+  ],
+  resort_images: [
+    {
+      id: '1',
+      name: 'Image 1',
+      alt: 'Image 1 alt text',
+      sort_order: 1,
+      image: {
+        path: '/path/to/image1.jpg',
+        content_type: 'image/jpeg',
+      },
+    },
+  ],
 };
 
 describe('Resort', () => {
@@ -39,45 +57,59 @@ describe('Resort', () => {
   });
 
   it('Has a resort title', () => {
-    cy.get('.resort-header__title').should('be.visible');
+    cy.get('.rating__title').should('be.visible').and('contain.text', dataProvider.title);
   });
 
   it('Has a resort country', () => {
-    cy.get('.resort-header__country-name').should('contain.text', 'Antarctica');
-  });
-
-  it('Has a resort state', () => {
-    cy.get('.resort-header__state-name').should('contain.text', 'Dont have a State ey bro');
+    cy.get('.resort-card__location-title').should('contain.text', dataProvider.location.country.name);
   });
 
   it('Has a description', () => {
-    cy.get('.resort__description-content').should('contain.text', 'Perpendicular curves to the ceiling for maximum air time');
+    cy.get('.resort-card__description-single-resort').should('contain.text', dataProvider.description);
   });
 
   it('Should render a rating Digital Nomad Score', () => {
-    cy.get('.rating__title')
-      .should('contain', 'Digital Nomad Score');
+    cy.get('.rating__title').should('contain', dataProvider.ratingScores[0].title);
   });
 
   it('Statistic should have a value', () => {
-    cy.get('.rating__number-big').should('contain', '76');
+    cy.get('.rating__number-big').should('contain', dataProvider.ratingScores[0].value);
   });
 
   it('Should render a numeric "Average Annual Snowfall"', () => {
-    cy.get('.statistic__title')
-      .should('contain', 'Average Annual Snowfall');
+    cy.get('.statistic__title').should('contain', dataProvider.numerics[0].title);
   });
 
   it('Should render a numeric value "Average Annual Snowfall"', () => {
-    cy.get('.statistic__number-big').should('contain', '10');
+    cy.get('.statistic__number-big').should('contain', dataProvider.numerics[0].value);
   });
 
-  it('Should render a generic "Snow Making"', () => {
-    cy.get('.generic__label').should('contain', 'Snow Making');
+  it('Should render images in the carousel', () => {
+    cy.get('.resort-single__image-carousel img').should('have.attr', 'src', dataProvider.resort_images[0].image.path);
   });
 
-  it('Should render a generic', () => {
-    cy.get('.generic__value').should('contain.text', 'true');
+  it('Should render comments section', () => {
+    cy.get('.resort-comments').should('be.visible');
+  });
+
+  it('Should render a comment author', () => {
+    cy.get('.carousel__author').should('contain.text', dataProvider.comments[0].author);
+  });
+
+  it('Should render a comment text', () => {
+    cy.get('.carousel__comment-text').should('contain.text', dataProvider.comments[0].comment);
+  });
+
+  it('Should render a map', () => {
+    cy.get('.resort__map-card').should('be.visible');
+  });
+
+  it('Should have a back button', () => {
+    cy.get('.resort.back-button').should('be.visible');
+  });
+
+  it('Back button should navigate back', () => {
+    cy.get('.resort.back-button').click();
+    cy.location('pathname').should('not.contain', '/resorts/pipedream');
   });
 });
-
--- src/stories/Resort/Resort.stories.js
+++ src/stories/Resort/Resort.stories.js
@@ -3,8 +3,7 @@ import { IntlProvider } from 'react-intl';
 import { MemoryRouter } from 'react-router';
 import { Route, Routes } from 'react-router-dom';
 import ResortComponent, { QUERY_RESORT } from '../../js/pages/Resort/Resort';
-import NoCacheMockedProvider
-  from '../../js/components/tests/NoCacheMockedProvider/NoCacheMockedProvider';
+import NoCacheMockedProvider from '../../js/components/tests/NoCacheMockedProvider/NoCacheMockedProvider';
 import langEn from '../../js/lang/en.json';
 
 export default {
@@ -28,8 +27,30 @@ export const Resort = () => {
             title: 'Pipe Dream',
             url_segment: 'pipedream',
             description: 'Perpendicular curves to the ceiling for maximum air time.',
+            resort_images: [
+              {
+                id: '1',
+                name: 'Image 1',
+                alt: 'Image 1 alt text',
+                sort_order: 1,
+                image: {
+                  path: '/path/to/image1.jpg',
+                  content_type: 'image/jpeg',
+                  __typename: 'ImageType',
+                },
+                __typename: 'ResortImageType',
+              },
+            ],
+            total_score: {
+              title: 'Total Score',
+              value: 85,
+              __typename: 'ScoreType',
+            },
             location: {
               id: '3',
+              latitude: -77.85,
+              longitude: 166.666,
+              city: 'McMurdo Station',
               country: {
                 id: '161',
                 code: 'NZ',
@@ -44,11 +65,12 @@ export const Resort = () => {
               },
               __typename: 'LocationType',
             },
-            ratings: [
+            ratingScores: [
               {
                 id: '2',
                 title: 'Digital Nomad Score',
                 value: 76,
+                name: 'digital_nomad_score',
                 __typename: 'Rating',
               },
             ],
@@ -57,6 +79,12 @@ export const Resort = () => {
                 id: '1',
                 title: 'Average Annual Snowfall',
                 value: 10,
+                name: 'average_annual_snowfall',
+                type: {
+                  unit: 'cm',
+                  max_value: 100,
+                  __typename: 'Type',
+                },
                 __typename: 'Numeric',
               },
             ],
@@ -65,7 +93,16 @@ export const Resort = () => {
                 id: '1',
                 title: 'Snow Making',
                 value: 'true',
+                name: 'snow_making',
                 __typename: 'Generic',
+              },
+            ],
+            comments: [
+              {
+                id: '1',
+                author: 'John Doe',
+                comment: 'Amazing place!',
+                __typename: 'Comment',
               },
             ],
             __typename: 'Resort',
@@ -77,7 +114,7 @@ export const Resort = () => {
 
   return (
     <MemoryRouter initialEntries={['/resorts/pipedream']}>
-      <IntlProvider locale="en" message={langEn}>
+      <IntlProvider locale="en" messages={langEn}>
         <Routes>
           <Route
             exact
@@ -86,7 +123,7 @@ export const Resort = () => {
               <NoCacheMockedProvider mocks={[mocks.resortByUrlSegment]}>
                 <ResortComponent />
               </NoCacheMockedProvider>
-  )}
+            )}
           />
         </Routes>
       </IntlProvider>
