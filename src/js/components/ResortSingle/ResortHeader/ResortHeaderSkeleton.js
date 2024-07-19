5aca2e6392e3ef72e43f37cb5a4c4a6eba867c0d
Thomas A Hansen <45204791+Krank3n@users.noreply.github.com>
2024-07-19 16:24:32 +1000

    Added in loading state for resort page

---
+++ src/js/components/ResortSingle/ResortHeader/ResortHeaderSkeleton.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  CCard, CCardBody, CCardHeader,
+} from '@coreui/react';
+
+const ResortHeaderSkeleton = () => (
+  <div className="resort-card--skeleton d-flex justify-content-center fade-in w-100">
+    <CCard className="resort-card__wrap collapsed w-100">
+      <CCardHeader className="resort-card__header-wrap pb-0 mb-2">
+        <div className="skeleton-card__header d-flex mb-3">
+          <div className="skeleton-rating-header me-3" />
+          <div className="skeleton-rating-title w-100" />
+        </div>
+        <div className="resort-card__location skeleton-location d-flex mb-2">
+          <div className="resort-card__country-flag-wrap country-flag-wrap me-3" />
+          <div className="skeleton-location-details w-50" />
+        </div>
+      </CCardHeader>
+      <CCardBody className="resort-card__body pt-0 pb-0 mb-3">
+        <div className="skeleton-content-wrap d-flex mb-2">
+          <div className="skeleton-sub-rating-wrap mt-2 me-2" />
+        </div>
+      </CCardBody>
+    </CCard>
+  </div>
+);
+
+export default ResortHeaderSkeleton;
