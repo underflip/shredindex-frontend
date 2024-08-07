42a0c2b0b2978d1b643d5be0f9cb290d1251b7a4
Thomas A Hansen <45204791+Krank3n@users.noreply.github.com>
2024-08-07 13:25:22 +1000

    Fixed filtering background homepage Fixed loading state in pages footer Updated RangeRheostatGraph.js

--- src/js/components/RankedResortFilters/RankedResortFilters.js
+++ src/js/components/RankedResortFilters/RankedResortFilters.js
@@ -252,6 +252,7 @@ const RankedResortFilters = () => {
                       );
                     }
                   }}
+                  useGraph
                 />
               )}
             </FilterToggleButton>
--- src/js/hooks/useQueryResortsRheostat.js
+++ src/js/hooks/useQueryResortsRheostat.js
@@ -1,33 +1,26 @@
-import { gql } from '@apollo/client';
-import { useQuery } from '@apollo/react-hooks';
+import { gql, useQuery } from '@apollo/client';
 
 export const QUERY_RESORTS_RHEOSTAT = gql`
-query Resorts($first: Int! $page: Int! $filter: Filter $orderBy: OrderBy){
-  resorts(
-    first: $first
-    page: $page
-    filter: $filter
-    orderBy: $orderBy
-  ) {
-    data {
-      ratingScores {
-        name
-        value
-      }
+query ResortRatingsForRheostat($typeName: String!) {
+    rheostatDirective(typeName: $typeName) {
+      min
+      max
+      tickers
+      totalResorts
+    }
+    types {
+      name
+      max_value
+      category
     }
   }
-}
 `;
 
-const useQueryResortsRheostat = (first, page, filter, orderBy) => useQuery(
+const useQueryResortsRheostat = (typeName) => useQuery(
   QUERY_RESORTS_RHEOSTAT,
   {
-    variables: {
-      first,
-      page,
-      filter,
-      orderBy,
-    },
+    variables: { typeName },
+    fetchPolicy: 'cache-first',
   },
 );
 
