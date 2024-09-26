aaeeda334adbc9658539a8e7be9c6132a07d38b2
Thomas A Hansen <45204791+Krank3n@users.noreply.github.com>
2024-09-26 15:05:00 +1000

    Fixed Linting

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require('cypress');
+import coverage from '@cypress/code-coverage/task'
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // eslint-disable-next-line import/extensions,global-require
+      coverage(on, config);
       return require('./cypress/plugins/index.js')(on, config);
     },
     baseUrl: 'http://localhost:6006',
