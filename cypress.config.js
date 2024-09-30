dd055c08a2433e7312ab027c2568206e534961f9
Thomas A Hansen <45204791+Krank3n@users.noreply.github.com>
2024-10-01 07:38:40 +1000

    Fix tests and linting

--- cypress.config.js
+++ cypress.config.js
@@ -9,7 +9,11 @@ module.exports = defineConfig({
     baseUrl: 'http://localhost:6006',
     testIsolation: false,
   },
-
+  env: {
+    NEXT_PUBLIC_MAPBOX_API_KEY: process.env.NEXT_PUBLIC_MAPBOX_API_KEY,
+    NEXT_PUBLIC_MAPBOX_STYLE: process.env.NEXT_PUBLIC_MAPBOX_STYLE,
+    NEXT_PUBLIC_GRAPHQL_ENDPOINT: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
+  },
   component: {
     devServer: {
       framework: 'react',
