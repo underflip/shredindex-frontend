f96eaedabacd7a0e2b78c53d5ee1e71e0feebd5b
Thomas A Hansen <45204791+Krank3n@users.noreply.github.com>
2024-10-19 10:33:29 +1000

    Attempt to fix font in deployment

--- pages/_document.js
+++ pages/_document.js
@@ -8,11 +8,8 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
         <link
-          rel="preload"
           href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;400;600;700;800;900&family=Roboto:wght@100;300;500;700;900&family=Plus+Jakarta+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
+          rel="stylesheet"
         />
       </Head>
       <body>
